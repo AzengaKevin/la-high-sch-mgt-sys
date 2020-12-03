1048ca1fa1d2ec17271ae96842aa878204d518bd
Azenga Kevin <azenga.kevin7@gmail.com>
2020-12-03 23:41:35 +0300

    Wip, added bootstrap SVG icons

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,4 +15,4 @@ mix.js('resources/js/app.js', 'public/js').postCss('resources/css/app.css', 'pub
     require('postcss-import'),
     require('tailwindcss'),
     require('autoprefixer'),
-]);
+]).copyDirectory('node_modules/bootstrap-icons/icons', 'public/icons');
